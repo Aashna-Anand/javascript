7e7edd7964aaa2f32fc35a2038954e2acbe4e8a4
Aashna-Anand <180502256+Aashna-Anand@users.noreply.github.com>
2025-10-16 10:09:59 +0000

    numbers and maths

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,14 @@
+const score =400
+console.log("Your score is " + score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+const otherNumber = 123.123456
+console.log(otherNumber.toPrecision(4))
+
+const hundreds = 100
+console.log(hundreds.toLocaleString("en-IN"))
