072b8fa73d5d39c7b5923295db97a49ec7ff508a
Aashna-Anand <180502256+Aashna-Anand@users.noreply.github.com>
2025-10-19 19:15:56 +0000

    nums and maths

--- 01_basics/07_nums_and_maths.js
+++ 01_basics/07_nums_and_maths.js
@@ -1,14 +1,31 @@
-const score =400
-console.log("Your score is " + score)
+// const score =400
+// console.log("Your score is " + score)
 
-const balance = new Number(100)
-console.log(balance)
+// const balance = new Number(100)
+// console.log(balance)
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(2))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
 
-const otherNumber = 123.123456
-console.log(otherNumber.toPrecision(4))
+// const otherNumber = 123.123456
+// console.log(otherNumber.toPrecision(4))
 
-const hundreds = 100
-console.log(hundreds.toLocaleString("en-IN"))
+// const hundreds = 100
+// console.log(hundreds.toLocaleString("en-IN"))
+
+// ======Maths ======
+
+console.log(Math);
+// console.log(Math.abs(-100));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2, 33, 4, 1, 55, -23));
+// console.log(Math.max(2, 33, 4, 1, 55, -23));
+// console.log(Math.random());
+const min = 10;
+const max = 200;
+
+ console.log(Math.floor(Math.random() * 10 + 1)); // random number between 1-10  
+// console.log(Math.pow(2, 4));
+console.log(Math.floor(Math.random() * max-min+1) + min); // random number between min - max
