3da81269f07da25f4a812d457c6a2e839e02f3c9
Aashna-Anand <180502256+Aashna-Anand@users.noreply.github.com>
2025-11-01 09:14:55 +0000

    foreach loop

---
+++ 04_control_flow/foreach_loop.js
@@ -0,0 +1,38 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);         // function create karke foreach me use kr skte h to phir foreach mein
+                                    // wapas se function define krne ki jarurat nhi 
+
+// }
+
+// coding.forEach(printMe)
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
