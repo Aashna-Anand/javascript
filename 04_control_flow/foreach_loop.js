085869a4e4ab800ebfdf86bb78d8bde8a4754369
Aashna-Anand <180502256+Aashna-Anand@users.noreply.github.com>
2025-11-01 09:39:32 +0000

    for each loop

--- 04_control_flow/foreach_loop.js
+++ 04_control_flow/foreach_loop.js
@@ -36,3 +36,19 @@ myCoding.forEach( (item) => {
     
     console.log(item.languageName);
 } )
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// } )
+
+// const newNums = []
+
+// myNums.forEach( (num) => {
+//     if (num > 4) {
+//         newNums.push(num)
+//     }
+// } )
+
+// console.log(newNums);
