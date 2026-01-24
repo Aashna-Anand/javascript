c52431a87df22913b2715bb02fb0532629a6d03e
Aashna-Anand <180502256+Aashna-Anand@users.noreply.github.com>
2026-01-24 16:53:29 +0000

    js

---
+++ revision.js
@@ -0,0 +1,15 @@
+// What is hoisting?
+
+// JS moves declarations to the top, not initializations.
+
+
+
+let obj1 = { x: 10 }
+let obj2 = obj1
+
+obj2.x = 99
+
+console.log(obj1.x) // 99 😱
+
+// primitive(copy by value) and non-primitive(copy by referrence)
+// == ===
